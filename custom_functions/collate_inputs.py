1552f7c59f43667eebbc899e4007cf7a488bd660
admin <root@localhost>
2022-05-26 14:00:14 +0000

    Custom Function "collate_inputs" imported.

---
+++ custom_functions/collate_inputs.py
@@ -0,0 +1,62 @@
+def collate_inputs(input0=None, input1=None, input2=None, input3=None, input4=None, input5=None, input6=None, input7=None, input8=None, headers=None, **kwargs):
+    """
+    Collates lists of inputs to each other using the python zip function, along with extending any lists that are shorter. This allows playbook inputs to be merged together and used in tandem downstream.
+    
+    Args:
+        input0 (CEF type: *)
+        input1 (CEF type: *)
+        input2 (CEF type: *)
+        input3 (CEF type: *)
+        input4 (CEF type: *)
+        input5 (CEF type: *)
+        input6 (CEF type: *)
+        input7 (CEF type: *)
+        input8 (CEF type: *)
+        headers: If headers are included, they will be used as key names for the outputs on top of the default output# naming and can be referenced by name instead of remembering what input goes to which output.
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        *.output0 (CEF type: *)
+        *.output1 (CEF type: *)
+        *.output2 (CEF type: *)
+        *.output3 (CEF type: *)
+        *.output4 (CEF type: *)
+        *.output5 (CEF type: *)
+        *.output6 (CEF type: *)
+        *.output7 (CEF type: *)
+        *.output8 (CEF type: *)
+        *.named_header: If headers are provided, they will also be used alongside the output#. For instance, if you pass in a list in the input0 and add `some_key` the output will be `{"output0": "some_value", "some_key": "some_value"}` to make it easier to reference downstream.
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    headers = headers or []
+    if headers:
+        headers = [h.strip() for h in headers.split(',')]
+        phantom.debug(f'Headers included = {headers}')
+    
+    # List of inputs
+    inputs = [input0, input1, input2, input3, input4, input5, input6, input7, input8]
+    # Make each input a list if it is not one yet
+    inputs = [inp if isinstance(inp, list) else [inp] for inp in inputs]
+    # Get the largest list length of the inputs
+    max_len = max(map(len, inputs))
+    # Extend each list to match the length of the longest list
+    inputs = [inp + [inp[-1]]*(max_len-len(inp)) for inp in inputs]
+    
+    outputs = []
+
+    for merged_inputs in zip(*inputs):
+        out_dict = {}
+        for idx, output in enumerate(merged_inputs):
+            if output is not None:
+                out_dict[f'output{idx}'] = output
+                # If headers is provided, then it will also be used in the output
+                if headers and idx <= len(headers):
+                    out_dict[headers[idx]] = output
+        
+        outputs.append(out_dict)
+
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs

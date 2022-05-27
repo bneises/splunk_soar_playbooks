d491235bf6029ef931bbc04bdb83ee9d45256979
admin <root@localhost>
2022-05-27 18:50:52 +0000

    initial save

---
+++ custom_functions/render_template.py
@@ -0,0 +1,41 @@
+def render_template(template=None, input0=None, input1=None, input2=None, input3=None, input4=None, input5=None, input6=None, input7=None, input8=None, **kwargs):
+    """
+    Args:
+        template
+        input0
+        input1
+        input2
+        input3
+        input4
+        input5
+        input6
+        input7
+        input8
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        rendered_text
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    context = {
+        "input0": input0,
+        "input1": input1,
+        "input2": input2,
+        "input3": input3,
+        "input4": input4,
+        "input5": input5,
+        "input6": input6,
+        "input7": input7,
+        "input8": input8
+    }
+    
+    outputs['rendered_text'] = phantom.render_template(template, context)
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
+                                
